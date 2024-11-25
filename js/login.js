3d46b0185e990e3c1f7e926c312ac4924e290cdd
Gabriela Foresti <gabriela.foresti93@gmail.com>
2024-11-24 23:43:10 -0300

    arreglo login.js para inicio sesión

--- js/login.js
+++ js/login.js
@@ -1,23 +1,18 @@
 document.getElementById('submit').addEventListener('click', () => {
-    const inputs = document.querySelectorAll('#form-login input'); // Selecciona todos los inputs del formulario
-    let resultado = '';
-    console.log(inputs);
+  const inputs = document.querySelectorAll('#form-login input'); // Selecciona todos los inputs del formulario
+  inputs.forEach((input) => {
+      const errorSpan = document.getElementById(`error-${input.name}`); // Busca el span de error correspondiente al input por su atributo "name"
 
-    inputs.forEach((input, index) => {
-        if (input.value.trim() === '') {
-           const span = document.getElementById('error-email')
-           span.classList.remove('hidden')
-           span.classList.add('span')
-         console.log(input.classList);
-          input.classList.add('input-error');
-          resultado += `El input ${index + 1} está vacío.<br>`;
-          console.log(input.classList);
-          console.log(resultado);
-        } else {
-          // Si el input tiene valor, elimina la clase de error
+      if (input.value.trim() === '') {
+          // Si el input está vacío, muestra el error y aplica el estilo
+          errorSpan.classList.remove('hidden');
+          errorSpan.classList.add('error-text');
+          input.classList.add('input-error'); // Marca el input con un borde de error
+      } else {
+          // Si el input tiene valor, oculta el error y elimina el estilo
+          errorSpan.classList.add('hidden');
+          errorSpan.classList.remove('error-text');
           input.classList.remove('input-error');
-          resultado += `El input ${index + 1} tiene valor: ${input.value}<br>`;
-        }
-      });
+      }
   });
-
+});
