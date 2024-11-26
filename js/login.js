8627c5a914c273bd9876a95bd51d9b7119201e45
Gabriela Foresti <gabriela.foresti93@gmail.com>
2024-11-26 10:14:44 -0300

    cambio text-area

--- js/login.js
+++ js/login.js
@@ -1,15 +1,15 @@
 document.getElementById('submit').addEventListener('click', () => {
-  const inputs = document.querySelectorAll('#form-login input'); // Selecciona todos los inputs del formulario
+  const inputs = document.querySelectorAll('#form-login input'); 
   inputs.forEach((input) => {
-      const errorSpan = document.getElementById(`error-${input.name}`); // Busca el span de error correspondiente al input por su atributo "name"
+      const errorSpan = document.getElementById(`error-${input.name}`);
 
       if (input.value.trim() === '') {
-          // Si el input está vacío, muestra el error y aplica el estilo
+          
           errorSpan.classList.remove('hidden');
           errorSpan.classList.add('error-text');
-          input.classList.add('input-error'); // Marca el input con un borde de error
+          input.classList.add('input-error'); 
       } else {
-          // Si el input tiene valor, oculta el error y elimina el estilo
+          
           errorSpan.classList.add('hidden');
           errorSpan.classList.remove('error-text');
           input.classList.remove('input-error');
