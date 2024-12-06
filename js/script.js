b724a234dd7870d7d54cfea68181a3c22e8cd7f2
Gabriela Foresti <gabriela.foresti93@gmail.com>
2024-12-05 23:22:59 -0300

    burguer menu

--- js/script.js
+++ js/script.js
@@ -10,6 +10,14 @@ document.getElementById("header").innerHTML = `
           <a href="form.html">Formulario Adopción</a>
           <a href="login.html">Iniciar sesión</a>
           <a href="registro.html">Registro</a>
+          <button class="burger-menu">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M4 6l16 0"></path>
+        <path d="M4 12l16 0"></path>
+        <path d="M4 18l16 0"></path>
+    </svg>
+</button>
         </nav>
          <div class="theme-switch">
   <input type="checkbox" id="theme-toggle" />
